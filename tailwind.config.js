377c07fd8b1401afb680a97d5eda49e1a6818e80
Bahadlm24 <bahadirperveli01@gmail.com>
2024-01-30 18:31:13 +0300

    son detaylar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container:{
+      center:true,
+      screens:{
+        lg:'1140px',
+        xl:'1140px',
+        '2xl':'1140px',
+      },
+    },
+    extend: {
+      fontFamily:{
+        gemunu :['Gemunu Libre', 'sans-serif'],
+        open:['Open Sans', 'sans-serif'],
+      },
+      
+        colors:{
+            'fe-red' : '#BC1A45',
+            'fe-melon' : '#FFD369',
+            'fe-grey' : '#DDDDDD',
+            'fe-white' : '#F7F7F7',
+        },
+
+        spacing:{
+          '128' : '32rem',
+        },
+    },
   },
   plugins: [],
 }
